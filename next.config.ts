fa23d7f73a5b8230e361505b66262f6aba87d5d5
sreeha <sreehareddy06@gmail.com>
2026-05-24 12:06:29 +0530

    chore: correct root-level location for allowedDevOrigins configuration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
+  ...({
     allowedDevOrigins: ["192.168.29.129"]
-  }
+  } as any)
 };
 
 export default nextConfig;
